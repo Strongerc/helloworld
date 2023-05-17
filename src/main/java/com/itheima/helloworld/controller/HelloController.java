769dae8cb7c2bd7bf6435efc3e02f4710e64bf6f
cc <206214729@qq.com>
2023-05-17 08:36:04 +0800

    test

---
+++ src/main/java/com/itheima/helloworld/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.itheima.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author cc
+ * @version V1.0
+ * @Package com.itheima.helloworld.controller
+ * @date 2023/5/16 23:23
+ */
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("")
+    public String hello() {
+        System.out.println("123");
+        return "OK";
+    }
+}
